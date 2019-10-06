0b30d6d6ae65826ad5ca2cc60472870e83556453
Erik Batista <erikbatista42@gmail.com>
2019-10-05 18:12:14 -0700

    added mongo and store model

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
 
 app = Flask(__name__)
+client = MongoClient()
+db = client.Store
+shirts = db.shirts
+
+# items = [
+#     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+# ]
+
+
 @app.route("/")
 def index():
-    return render_template('home.html', msg="This is pretty cool")
+    return render_template('home.html', shirts=shirts.find())
 
 if __name__ == "__main__":
     app.run(debug=True)
