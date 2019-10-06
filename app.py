8c164b44ee32db8ae04aebabe3c271ecb53874ac
Erik Batista <erikbatista42@gmail.com>
2019-10-05 20:24:52 -0700

    create function added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 
@@ -6,12 +6,6 @@ app = Flask(__name__)
 client = MongoClient()
 db = client.Store
 shirts = db.shirts
-
-# items = [
-#     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-# ]
-
 
 @app.route("/")
 def index():
@@ -21,6 +15,18 @@ def index():
 def shirts_new():
     # create new shirt to sell
     return render_template("shirts_new.html")
+
+@app.route('/shirts', methods=['POST'])
+def shirt_upload():
+    # upload new shirt
+    shirt = {
+        'title': request.form.get('title'),
+        'image': request.form.get('image').split(),
+        'price': request.form.get('price').split(),
+        'description': request.form.get('description')
+    }
+    shirts.insert_one(shirt)
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
